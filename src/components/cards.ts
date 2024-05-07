2aa2086b9f1a7c5fd13d06e49d071cc34323b4eb
sam <84742666+samsvk@users.noreply.github.com>
2024-05-07 18:16:34 +0100

    ficxed a lot

--- src/components/cards.ts
+++ src/components/cards.ts
@@ -2,7 +2,7 @@ import { card } from "./card";
 
 export function renderCards() {
   return `<div id="cards" class="relative w-full gap-6 items-center justify-center p-4 md:max-w-4xl max-w-[460px] grid md:grid-cols-3 grid-cols-1 mx-auto min-h-5 md:mt-12 mb-28">
-    <div class="md:-rotate-[8deg] md:translate-y-6 md:z-0 md:relative md:translate-x-8">${card(
+    <a href="https://dashboard.shrouded.app/pricing?ref=landing&type=free" class="md:-rotate-[8deg] md:translate-y-6 md:z-0 md:relative md:translate-x-8">${card(
       "Free",
       0,
       "Great for individuals who have minimal online presence.",
@@ -13,39 +13,39 @@ export function renderCards() {
         "Single sign-on",
         "Email support",
       ]
-    )}</div>
-    <div class="md:-rotate-[1deg] md:-translate-y-12 md:z-1 md:relative">${card(
-      "Great value",
-      3.99,
-      "Billed monthly great for individuals who use multiple online services.",
-      "Premium",
-      [
-        "Create unlimited aliases",
-        "Update unlimited aliases",
-        "Chrome Extension",
-        "Destination control",
-        "Cancel anytime",
-        "Single sign-on",
-        "24/7 support",
-        "Spam filter",
-      ],
-      "primary"
-    )}</div>
-    <div class="md:rotate-[12deg] md:z-2 md:relative md:translate-y-2 md:-translate-x-10">${card(
-      "One time payment",
-      49.99,
-      "One time payment great for families who use multiple online services.",
-      "Unlimited access",
-      [
-        "Create unlimited aliases",
-        "Update unlimited aliases",
-        "Destination control",
-        "Chrome extension",
-        "Lifetime access",
-        "Single sign-on",
-        "24/7 support",
-        "Spam filter",
-      ]
-    )}</div>
+    )}</a>
+     <a href="https://dashboard.shrouded.app/pricing?ref=landing&type=premium"  class="md:-rotate-[1deg] md:-translate-y-12 md:z-1 md:relative">${card(
+       "Great value",
+       3.99,
+       "Billed monthly great for individuals who use multiple online services.",
+       "Premium",
+       [
+         "Create unlimited aliases",
+         "Update unlimited aliases",
+         "Chrome Extension",
+         "Destination control",
+         "Cancel anytime",
+         "Single sign-on",
+         "24/7 support",
+         "Spam filter",
+       ],
+       "primary"
+     )}</a>
+     <a href="https://dashboard.shrouded.app/pricing?ref=landing&type=unlimited"  class="md:rotate-[12deg] md:z-2 md:relative md:translate-y-2 md:-translate-x-10">${card(
+       "One time payment",
+       49.99,
+       "One time payment great for families who use multiple online services.",
+       "Unlimited access",
+       [
+         "Create unlimited aliases",
+         "Update unlimited aliases",
+         "Destination control",
+         "Chrome extension",
+         "Lifetime access",
+         "Single sign-on",
+         "24/7 support",
+         "Spam filter",
+       ]
+     )}</a>
   </div>`;
 }
