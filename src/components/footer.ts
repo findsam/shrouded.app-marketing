eb52d20c429d62aed604ae2f8de4f4252947fc05
sam <84742666+samsvk@users.noreply.github.com>
2024-05-08 13:56:37 +0100

    Update footer.ts

--- src/components/footer.ts
+++ src/components/footer.ts
@@ -5,18 +5,21 @@ export default function footer() {
               <li class="text-sm leading-normal tracking-tight text-left flex gap-2 items-center text-primary font-semibold  mr-auto">Company
             </li>
             <li class="text-sm font-normal text-secondary leading-normal tracking-tight text-left flex gap-2 items-center mr-auto">©Shrouded.app @2024
-            <li class="text-sm font-normal text-secondary leading-normal tracking-tight text-left flex gap-2 items-center mr-auto">
-            Terms and Conditions</li>
+      <li class="text-sm font-normal text-secondary leading-normal tracking-tight text-left flex gap-2 items-center mr-auto hover:cursor-pointer hover:text-primary duration-150"><a href="https://notion.so" target="_blank">Terms and Conditions</a>
+          </li>
             </li>
-                <li class="text-sm font-normal text-secondary leading-normal tracking-tight text-left flex gap-2 items-center mr-auto">Cookie Policy
-            </li>
-            <li class="text-sm font-normal text-secondary leading-normal tracking-tight text-left flex gap-2 items-center mr-auto">
-           Privacy Policy</li>
+            <li class="text-sm font-normal text-secondary leading-normal tracking-tight text-left flex gap-2 items-center mr-auto hover:cursor-pointer hover:text-primary duration-150"><a href="https://notion.so" target="_blank">Cookie policy</a>
+          </li>
+      <li class="text-sm font-normal text-secondary leading-normal tracking-tight text-left flex gap-2 items-center mr-auto hover:cursor-pointer hover:text-primary duration-150"><a href="https://notion.so" target="_blank">Privacy policy</a>
+          </li>
 
         <li class="text-sm  leading-normal tracking-tight text-left flex gap-2 items-center mr-auto  text-primary font-semibold mt-4">Socials
             </li>
-         <li class="text-sm font-normal text-secondary leading-normal tracking-tight text-left flex gap-2 items-center mr-auto">Twitter</li>
-           <li class="text-sm font-normal text-secondary leading-normal tracking-tight text-left flex gap-2 items-center mr-auto">LinkedIn</li>
+      <li class="text-sm font-normal text-secondary leading-normal tracking-tight text-left flex gap-2 items-center mr-auto hover:cursor-pointer hover:text-primary duration-150">
+      <a href="https://twitter.com" target="_blank">Twitter</a>
+          </li>
+           <li class="text-sm font-normal text-secondary leading-normal tracking-tight text-left flex gap-2 items-center mr-auto hover:cursor-pointer hover:text-primary duration-150"><a href="https://linkedin.com" target="_blank">LinkedIn</a>
+          </li>
         </ul>
         <ul class="w-full py-2 flex flex-col gap-1 justify-start items-start col-span-2 max-w-32 ml-auto">
             <li class="text-sm  leading-normal tracking-tight text-left flex gap-2 items-center mr-auto  text-primary font-semibold ">Built with love in
