460c183719d332b74da280369736d009a61e6935
sam <84742666+samsvk@users.noreply.github.com>
2024-05-08 14:07:37 +0100

    Update footer.ts

--- src/components/footer.ts
+++ src/components/footer.ts
@@ -4,7 +4,7 @@ export default function footer() {
         <ul class="w-full py-2 flex flex-col gap-1 justify-start items-start col-span-2">
               <li class="text-sm leading-normal tracking-tight text-left flex gap-2 items-center text-primary font-semibold  mr-auto">Company
             </li>
-            <li class="text-sm font-normal text-secondary leading-normal tracking-tight text-left flex gap-2 items-center mr-auto">©Shrouded.app @2024
+            <li class="text-sm font-normal text-secondary leading-normal tracking-tight text-left flex gap-2 items-center mr-auto">©Shrouded.app 2024
       <li class="text-sm font-normal text-secondary leading-normal tracking-tight text-left flex gap-2 items-center mr-auto hover:cursor-pointer hover:text-primary duration-150"><a href="https://notion.so" target="_blank">Terms and Conditions</a>
           </li>
             </li>
